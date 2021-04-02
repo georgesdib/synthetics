7bb94aeb6f27c29c5ae28792294915b6f96204f6
Georges Dib <georges.dib@gmail.com>
2021-04-02 18:49:50 +0100

    Adding balances, and more tests

--- lib.rs
+++ lib.rs
@@ -48,11 +48,11 @@ mod synthetics {
         short: Token,
         /// The long token, you ming this when you buy the corresponding short
         /// long total balance has to be less than or equal than the total balance of short
-        long: Token,
-        // Collateral balance, TODO
+        long: Token
     }
 
     #[ink(event)]
+    /// Emitted when a Token is minted
     pub struct TokenMinted {
         #[ink(topic)]
         creator: AccountId,
@@ -60,12 +60,22 @@ mod synthetics {
         value: Balance
     }
 
+    // TODO: make this call an oracle, check return type, add asset type
+    pub fn get_price() -> u128 {
+        1
+    }
+
     impl Synthetics {
-        #[ink(constructor)]
+        #[ink(constructor, selector = "0xCAFEBABE")]
         /// When a contract is first created, a short is minted with the balance given
         pub fn new(initial_supply: Balance) -> Self {
             // TODO: how do I add the margin, start by taking the margin here
             let caller = Self::env().caller();
+            // Take the IM, if this fails don't continue the rest
+            // TODO make the HC percentage a parameter
+            let im = initial_supply * get_price() / 5;
+            // TODO Should I panic?
+            assert!(im <= Self::env().transferred_balance());
             let short = Token::new(initial_supply, caller);
             Self::env().emit_event(TokenMinted {
                 creator: caller,
@@ -101,21 +111,75 @@ mod synthetics {
     mod tests {
         use super::*;
 
+        use ink_env::{
+            call,
+            test,
+        };
         use ink_lang as ink;
 
         /// We test if the constructor does its job.
         #[ink::test]
         fn constructur_works() {
+            let account = create_contract(100);
             let synthetics = Synthetics::new(100);
 
             // TODO test events
             assert_eq!(synthetics.short_total_balance(), 100);
             assert_eq!(synthetics.long_total_balance(), 0);
 
-            let account = AccountId::from([0x01; 32]);
-
             assert_eq!(synthetics.short_balance_of(account), 100);
             assert_eq!(synthetics.long_balance_of(account), 0);
+        }
+
+        /// Failure for not enough balance
+        #[ink::test]
+        #[should_panic]
+        fn constructur_fails_no_balance() {
+            create_contract(1);
+            Synthetics::new(100);
+        }
+
+
+        // Helper functions
+        fn default_accounts(
+        ) -> ink_env::test::DefaultAccounts<ink_env::DefaultEnvironment> {
+            ink_env::test::default_accounts::<ink_env::DefaultEnvironment>()
+                .expect("Off-chain environment should have been initialized already")
+        }
+
+        fn contract_id() -> AccountId {
+            ink_env::test::get_current_contract_account_id::<ink_env::DefaultEnvironment>(
+            )
+            .expect("Cannot get contract id")
+        }
+
+        fn set_sender(sender: AccountId) {
+            let callee = ink_env::account_id::<ink_env::DefaultEnvironment>()
+                .unwrap_or([0x0; 32].into());
+            test::push_execution_context::<Environment>(
+                sender,
+                callee,
+                1000000,
+                1000000,
+                test::CallData::new(call::Selector::new([0x00; 4])), // dummy
+            );
+        }
+
+        fn create_contract(initial_balance: Balance) -> AccountId {
+            let accounts = default_accounts();
+            set_sender(accounts.alice);
+            let mut data = ink_env::test::CallData::new(ink_env::call::Selector::new([
+                0xCA, 0xFE, 0xBA, 0xBE,
+            ]));
+            data.push_arg(&accounts.alice);
+            ink_env::test::push_execution_context::<ink_env::DefaultEnvironment>(
+                accounts.alice,
+                contract_id(),
+                1000000,
+                initial_balance,
+                data,
+            );
+            accounts.alice
         }
     }
 }
